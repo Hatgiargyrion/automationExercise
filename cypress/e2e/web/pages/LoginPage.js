511eac859e3970f09df83123ca281cbd032ef7c3
Matheus <hatgiargyrion@hotmail.com>
2025-07-21 14:49:50 -0300

    Adicionando cenário de login com sucesso

--- cypress/e2e/web/pages/LoginPage.js
+++ cypress/e2e/web/pages/LoginPage.js
@@ -5,19 +5,43 @@ class LoginPage {
     }
 
     fillBasicSignupForm() {
-        elements.nameInput().type('Test User');
-        elements.emailInput().type(`testuser${Date.now()}@example.com`);
-        elements.signupButton().click();
-      }
+        elements.nameSignupInput().type('Test User');
+        elements.emailSignupInput().type(`testuser${Date.now()}@example.com`);
+    }
 
+    fillLoginForm(userData) {
+        elements.emailLoginInput().clear().type(userData.email);
+        elements.passwordLoginInput().clear().type(userData.password);
+    }
+
+    submitLogin() {
+        elements.loginButton().click();
+        
+    }
+
+    submitSignup() {
+        elements.signupButton().click();
+    }
+
+    verifyUserLogged() {
+        elements.userLoggedLabel().should('be.visible');
+
+        cy.get('@userData').then((userData) => {
+            elements.nameUserLoggedLabel(userData.name);
+        })
+    }
 }
 
 const elements = {
     loginTitle: () => cy.get('div h2').contains('Login to your account'),
-    nameInput: () => cy.get('[name="name"]'),
-    emailInput: () => cy.get('.signup-form [name="email"]'),
-    signupButton: () => cy.get('div.signup-form button')
-
+    nameSignupInput: () => cy.get('[data-qa="signup-name"]'),
+    emailSignupInput: () => cy.get('[data-qa="signup-name"]'),
+    emailLoginInput: () => cy.get('[data-qa="login-email"]'),
+    passwordLoginInput: () => cy.get('[data-qa="login-password"]'),
+    signupButton: () => cy.get('div.signup-form button'),
+    loginButton: () => cy.get('div.login-form button'),
+    userLoggedLabel: () => cy.get('a').contains(' Logged in as '),
+    nameUserLoggedLabel: (userData) => cy.get('b').contains(userData)
 }
 
 export default LoginPage
--- cypress/e2e/web/steps/login.steps.js
+++ cypress/e2e/web/steps/login.steps.js
@@ -1,4 +1,4 @@
-import { Given } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 import LoginPage from '../pages/LoginPage';
 
 const loginPage = new LoginPage();
@@ -6,4 +6,23 @@ const loginPage = new LoginPage();
 Given('preenche o pré-formulário com nome e e-mail', () => {
     loginPage.verifyLoginHeader();
     loginPage.fillBasicSignupForm();
+});
+
+Given('o usuário preenche o formulário de login com credenciais válidas do arquivo {string}', (fixturePath) => {
+    cy.fixture(fixturePath).then((userData) => {
+        cy.wrap(userData).as('userData')
+        loginPage.fillLoginForm(userData);
+    });
+});
+
+When('submete o login', () => {
+    loginPage.submitLogin();
+});
+
+When('submete o cadastro', () => {
+    loginPage.submitSignup();
+});
+
+Then('ele deve ser autenticado com sucesso', () => {
+    loginPage.verifyUserLogged();
 });
