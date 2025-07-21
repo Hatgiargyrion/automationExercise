8336b0021d7066e46219e6c1d0ca0752c56fe823
Matheus <hatgiargyrion@hotmail.com>
2025-07-21 08:27:25 -0300

    Ajustando implementação Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,27 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "cypress/e2e/**/*.feature",
+    supportFile: false,
+    setupNodeEvents: async (on, config) => {
+
+      config.env = config.env || {};
+      config.env.stepDefinitions = "cypress/e2e/web/steps/**/*.{js,ts}";
+
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
     },
   },
 });
